fcdb67d66140000eaccb46f77be7e3af82d6d9af
Mayen Akpan <neroxma@yahoo.com>
2022-03-03 15:29:28 -0800

    updated

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
+
+// set date input min with todays date
+const tday = new Date().toISOString();
+console.log(today);
