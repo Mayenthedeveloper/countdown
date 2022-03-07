35769204343eb967d3399fa0583692bf8bc4c029
Mayen Akpan <neroxma@yahoo.com>
2022-03-06 20:45:09 -0800

    updated form value

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // set date input min with todays date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
 
 //take values form input
 function updateCountdown(e) {
-  console.log(e);
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+
+  console.log(countdownDate, countdownTitle);
 }
 
 // event listener
