2145a83ba9781e3918144eb57add86928bc7a175
Mayen Akpan <neroxma@yahoo.com>
2022-03-08 20:02:38 -0800

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
+const countdownEl = document.getElementById("countdown");
+const countdownElTitle = document.getElementById("coutdown-title");
 
 let countdownTitle = "";
 let countdownDate = "";
