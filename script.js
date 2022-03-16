e3f602ddcd1f3036d3d9012a58de0f90593d077c
Mayen Akpan <neroxma@yahoo.com>
2022-03-15 20:49:17 -0700

    updated

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-const inputContainer = document.getElementById("input-container");
-const countdownForm = document.getElementById("countdownForm");
-const dateEl = document.getElementById("date-picker");
-const countdownEl = document.getElementById("countdown");
-const countdownElTitle = document.getElementById("coutdown-title");
-const countdownElBtn = document.getElementById("coutdown-button");
-const timeElements = document.querySelectorAll("span");
-
-let countdownTitle = "";
-let countdownDate = "";
-let countdownValue = Date;
-let couuntdownActive;
-
-const second = 1000;
-const min = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
 const countdownForm = document.getElementById("countdownForm");
 const inputContainer = document.getElementById("input-container");
 const dateEl = document.getElementById("date-picker");
@@ -125,53 +108,3 @@ completeBtn.addEventListener("click", reset);
 
 // On Load, check localStorage
 restorePreviousCountdown();
-
-// set date input min with todays date
-const today = new Date().toISOString().split("T")[0];
-dateEl.setAttribute("min", today);
-
-// Populate Countdown / Complete UI
-function updateDOM() {
-  countdownActive = setInterval(() => {
-    const now = new Date().getTime();
-    const distance = countdownValue - now;
-    const days = Math.floor(distance / day);
-    const hours = Math.floor((distance % day) / hour);
-    const minutes = Math.floor((distance % hour) / minute);
-    const seconds = Math.floor((distance % minute) / second);
-    // Hide Input
-    inputContainer.hidden = true;
-    // If the countdown has ended, show final state
-    if (distance < 0) {
-      countdownEl.hidden = true;
-      clearInterval(countdownActive);
-      completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
-      completeEl.hidden = false;
-    } else {
-      // else, show the countdown in progress
-      countdownElTitle.textContent = `${countdownTitle}`;
-      timeElements[0].textContent = `${days}`;
-      timeElements[1].textContent = `${hours}`;
-      timeElements[2].textContent = `${minutes}`;
-      timeElements[3].textContent = `${seconds}`;
-      completeEl.hidden = true;
-      countdownEl.hidden = false;
-    }
-  }, second);
-}
-
-//take values form input
-function updateCountdown(e) {
-  e.preventDefault();
-  countdownTitle = e.srcElement[0].value;
-  countdownDate = e.srcElement[1].value;
-
-  console.log(countdownDate, countdownTitle);
-  // get number version of current date, updateDOM
-  countdownValue = new Date(countdownDate).getTime();
-  console.log(countdownValue, "countdownValue");
-  updateDOM();
-}
-
-// event listener
-countdownForm.addEventListener("submit", updateCountdown);
